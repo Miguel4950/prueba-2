9b7cab92d3ae1c9e2706385f39a759657669fc53
Miguel4950 <miguelacu8@gmail.com>
2025-03-17 15:58:24 -0500

    Update Drops.sql

---
+++ Drops.sql
@@ -0,0 +1,16 @@
+-- Eliminar vistas
+DROP VIEW Punto_1;
+DROP VIEW Punto_2;
+DROP VIEW Punto_3;
+DROP VIEW Punto_4;
+DROP VIEW Punto_5;
+DROP VIEW Punto_6;
+DROP VIEW Punto_7;
+DROP VIEW Punto_8;
+DROP VIEW Punto_10;
+
+-- Eliminar tablas
+DROP TABLE abono;
+DROP TABLE prestamo;
+DROP TABLE deudor;
+DROP TABLE banco;
